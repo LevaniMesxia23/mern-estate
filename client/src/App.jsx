5dd52ce8a529d4c869989bddbe623c8162ee54f4
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-23 21:29:45 +0400

    feat: create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignUp from "./pages/SignUp"
+import Signin from "./pages/Signin"
 function App() {
 
   return (
     <>
-        <p className=' text-red-500'>rame</p>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="profile/" element={<Profile />}/>
+            <Route path="/sign-in" element={<Signin />}/>
+            <Route path="/sign-up" element={<SignUp />}/>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
