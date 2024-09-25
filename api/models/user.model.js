659e5b7009b23fde121ba97e0e8320a42a61a0b0
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-09-25 19:37:10 +0400

    feat: create and add Oauth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -15,6 +16,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     require: true,
   }, 
+  avatar:{
+    type: String,
+    default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+  },
 },{timestamps: true})
 
 const User = mongoose.model('User', userSchema)
