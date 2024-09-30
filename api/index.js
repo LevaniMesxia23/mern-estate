c183d6af0608e64b2893a8fead55d68adadf1936
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-10-01 00:23:54 +0400

    feat: Create update user API route

--- api/index.js
+++ api/index.js
@@ -1,29 +1,32 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "../api/routes/user.route.js"
-import authRouter from "../api/routes/auth.route.js"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connect to MongoDB!");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
+app.use(cookieParser());
 
-
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Server listens to port ${PORT}!`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
